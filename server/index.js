e57e75738102ee0bd9a438a8e207f6c2dd7a11cf
Giả Võ <vodangngocgia@gmail.com>
2021-03-05 09:43:53 +0700

    first commit

---
+++ server/index.js
@@ -0,0 +1,56 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const mongoose = require("mongoose");
+const FriendModel = require('./models/Friends');
+require('dotenv').config();
+
+app.use(cors());
+app.use(express.json());
+
+mongoose.connect(
+    "mongodb://localhost:27017/reactAndNode",
+    { useNewUrlParser: true }
+);
+
+app.post('/addFriend', async (req, res) => {
+    const name = req.body.name;
+    const age = req.body.age;
+
+    const friend = new FriendModel({name : name, age: age});
+    await friend.save();
+    res.send("Inserted Data")
+})
+
+app.get('/read', async (req, res) => {
+    FriendModel.find({}, (err, result) => {
+        if(err){
+            res.send(err)
+        }else{
+            res.send(result)
+        }
+    });
+});
+
+app.put('/update', async (req, res) =>{
+    const newAge = req.body.newAge;
+    const id = req.body.id;
+
+    try {
+        await FriendModel.findById(id, (error, friendToUpdate) =>{
+            friendToUpdate.age = Number(newAge);
+            friendToUpdate.save();
+        })
+    } catch (err) {
+        console.log(err)
+    }
+    res.send("updated");
+})
+app.delete('/delete/:id', async(req, res) =>{
+    const id = req.params.id;
+    await FriendModel.findByIdAndRemove(id).exec();
+    res.send("item deleted")
+})
+app.listen(process.env.PORT || 3001, () => {
+    console.log("You are connected!");
+})
